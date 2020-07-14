a64d2b30782ffb61948f469a852fca279966443b
Valeriy <requiemnosoul@gmail.com>
2020-07-15 00:56:40 +0500

    Initial commit

---
+++ kmz5/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace kmz5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 68);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(274, 267);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(340, 68);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(274, 267);
+            this.textBox2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 341);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(274, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Crypt/Decrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(287, 68);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(47, 267);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "---> <---";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(207, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(251, 22);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TextBox3_KeyUp);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(207, 40);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(251, 22);
+            this.textBox4.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Текстовое представление:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "16-ричное представление:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(477, 15);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 23);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Random Key";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(462, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Размер ключа: 0 бит";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 379);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "RC4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ kmz5/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kmz5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        byte [] K;
+        int[] S;
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+            textBox4.Clear();
+            RandomKey();
+        }
+
+        void Sblock()
+        {
+            S = new int[256];
+            for (int i = 0; i < 256; i++)
+            {
+                S[i] = i;
+            }
+            int t = 0;
+            for (int i = 0; i < S.Length; i++)
+            {
+                int x = S[i];
+                t = (t + S[i] + K[i%K.Length])%256;
+                S[i] = S[t];
+                S[t] = x;
+            }
+        }
+        byte slovoK (int a)
+        {
+            int x = 0;
+            int y = 0;
+            byte Slovo = 0;
+            for (int i = 0; i < a; i++)
+            {
+                x = (x + 1) % 256;
+                y = (y + S[x]) % 256;
+                int z = S[x];
+                S[x] = S[y];
+                S[y] = z;
+                int t = (S[x] + S[y]) % 256;
+                Slovo = Convert.ToByte(S[t]);
+            }
+            return (Slovo);
+        }
+
+        string Crypt()
+        {
+            string text = textBox1.Text;
+            byte[] b = Encoding.GetEncoding(1251).GetBytes(text);
+            for (int i = 0; i < text.Length; i++)
+            {
+                b[i] = XOR(Convert.ToByte(b[i]), slovoK(b.Length));
+            }
+            return (Encoding.GetEncoding(1251).GetString(b));
+        }
+        byte XOR(byte b1, byte b2)
+        {
+            string res = "";
+            string s1 = Convert.ToString(Convert.ToInt32(b1), 2);
+            string s2 = Convert.ToString(Convert.ToInt32(b2), 2);
+            while(s1.Length != 8 || s2.Length != 8)
+            {
+                if (s1.Length != 8)
+                    s1 = "0" + s1;
+                if (s2.Length != 8)
+                    s2 = "0" + s2;
+            }
+            for (int i = 0; i < 8; i++)
+            {
+                res += ((Convert.ToInt32(s1[i].ToString()) + Convert.ToInt32(s2[i].ToString())) % 2).ToString();
+            }
+            return (Convert.ToByte(res,2));
+        }
+        void RandomKey()
+        {
+            string key = "";
+            Random rnd = new Random();
+            int x = rnd.Next(20, 1025);
+            while (x % 8 != 0)
+                x++;
+            x *= 2;
+            for (int i = 0; i < x; i++)
+            {
+                key += Convert.ToString(rnd.Next(0, 2));
+            }
+            K = new byte[x/8];
+            for (int i = 0; i < x/8; i++)
+            {
+                string tmp = "";
+                for (int j = 0; j < 8; j++)
+                {
+                    tmp += key[i * 8 + j];
+                }
+                K[i] = Convert.ToByte(Convert.ToInt32(tmp,2));
+            }
+            for (int i = 0; i < K.Length; i++)
+            {
+                textBox3.Text += Convert.ToChar(K[i]);
+                string tmp = K[i].ToString("x");
+                if (tmp.Length < 2)
+                    tmp = "0" + tmp;
+                textBox4.Text += tmp;
+            }
+            label3.Text = "Размер ключа: " + x + " бит";
+        }
+        void enterKey()
+        {
+            string Hex = textBox4.Text;
+            K = new byte[Hex.Length / 2];
+            string[] tmp = new string[Hex.Length / 2];
+            for (int i = 0; i < Hex.Length / 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    tmp[i] += Hex[i * 2 + j];
+                }
+                K[i] = Convert.ToByte(Convert.ToInt32(tmp[i], 16));
+            }
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            enterKey();
+            Sblock();
+            textBox2.Text = Crypt();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox2.Text;
+            textBox2.Clear();
+        }
+
+        private void TextBox3_KeyUp(object sender, KeyEventArgs e)
+        {
+            textBox4.Clear();
+            byte[] tmp = Encoding.GetEncoding(1251).GetBytes(textBox3.Text);
+            for (int i = 0; i < tmp.Length; i++)
+            {
+                textBox4.Text += Convert.ToString(tmp[i], 16);
+            }
+            label3.Text = "Размер ключа: " + tmp.Length * 8 + " бит";
+        }
+    }
+}
